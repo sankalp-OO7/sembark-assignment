11477e4393397c1fd3b0f6cd9c580cd35e117835
Sankalp <your.email@example.com>
2025-11-04 11:00:51 +0530

    logo,svg addition

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import { AppBar, Toolbar, Typography, Button, Badge } from "@mui/material";
+import { AppBar, Toolbar, Typography, Button, Badge, Box } from "@mui/material";
 import { Link as RouterLink } from "react-router-dom";
 import { CartContext } from "../context/CartContext";
 import { observer } from "mobx-react-lite";
@@ -8,21 +8,55 @@ const Navbar: React.FC = observer(() => {
   const cart = useContext(CartContext);
 
   return (
-    <AppBar position="static">
-      <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
-        <Typography
-          variant="h6"
+    <AppBar position="static" color="primary">
+      <Toolbar
+        sx={{
+          display: "flex",
+          justifyContent: { xs: "space-between", sm: "space-between" },
+          mx: { xs: 1, sm: 3, md: 6, lg: 16 },
+        }}
+      >
+        {/* Left section: Logo + Brand name */}
+        <Box
           component={RouterLink}
           to="/"
-          sx={{ color: "inherit", textDecoration: "none" }}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+            textDecoration: "none",
+            color: "inherit",
+          }}
         >
-          Sembark Store
-        </Typography>
+          <Box
+            component="img"
+            src="https://media.licdn.com/dms/image/v2/D4E0BAQFJqUF4V0qDuw/company-logo_200_200/B4EZjmRpkOGYAI-/0/1756210043681/sembark_logo?e=2147483647&v=beta&t=QUotPrF1b7iQT4F4-CQKF5FuY-vrsTaEBOHUIOnr-9k"
+            alt="Sembark Logo"
+            sx={{
+              width: 90,
+              height: 50,
+              objectFit: "cover",
+              objectPosition: "center 50%", // crops top/bottom 20%
+              borderRadius: "8px",
+              mr: 1.5,
+            }}
+          />
+          <Typography
+            variant="h6"
+            sx={{
+              fontWeight: 600,
+              display: { xs: "none", sm: "block" },
+            }}
+          >
+            Sembark Store
+          </Typography>
+        </Box>
 
+        {/* Right section: Cart */}
         <Button
           component={RouterLink}
           to="/cart"
           color="inherit"
+          variant="outlined"
           startIcon={<Badge badgeContent={cart.totalItems} color="secondary" />}
           sx={{ textTransform: "none" }}
         >
